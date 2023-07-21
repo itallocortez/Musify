d399ee1307f4d9ca2f34fa3a03901855a852a230
Itallo Cortez <itallo.cortez.703@ufrn.edu.br>
2023-07-20 21:10:25 -0300

    Cria músicas e playlists para teste

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
-#include <iostream>
-
-using namespace std;
+#include "interface.hpp"
 
 int main() {
-	cout << "Hello World!" << endl;
+  Lista<Playlist*> playlists;
+  Lista<Musica*> musicas;
+
+  Playlist *play1 = new Playlist("Pop");
+  Playlist *play2 = new Playlist("Metal");
+  Musica *musi1 = new Musica("Uptown Funk", "Bruno Mars");
+  Musica *musi2 = new Musica("Someone Like You", "Adele");
+  Musica *musi3 = new Musica("Shape of You", "Ed Sheera");
+  Musica *musi4 = new Musica("Paranoid", "Black Sabbath");
+  Musica *musi5 = new Musica("Master of Puppets", "Metallica");
+
+  playlists.inserirNoFinal(play1);
+  playlists.inserirNoFinal(play2);
+  musicas.inserirNoFinal(musi1);
+  musicas.inserirNoFinal(musi2);
+  musicas.inserirNoFinal(musi3);
+  musicas.inserirNoFinal(musi4);
+  musicas.inserirNoFinal(musi5);
+
+  playlists.em(0)->getValor()->adicionarMusica(musi1);
+  playlists.em(0)->getValor()->adicionarMusica(musi2);
+  playlists.em(0)->getValor()->adicionarMusica(musi3);
+  playlists.em(1)->getValor()->adicionarMusica(musi4);
+  playlists.em(1)->getValor()->adicionarMusica(musi5);
+
+  while (menuPrincipal(&playlists, &musicas)) {
+  }
+
+  playlists.limparListaDePonteiros();
+  musicas.limparListaDePonteiros();
+
+  return 0;
 }
